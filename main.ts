ef97204935bf98e6c50b0af947ec27b6329be2a6
Daniel <70607383+danielthehecker@users.noreply.github.com>
2022-06-02 10:14:00 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let Drunk_Driver = sprites.create(assets.image`car20`, SpriteKind.Player)
+Drunk_Driver.setPosition(128, 65)
+scene.setBackgroundImage(sprites.background.cityscape2)
+controller.moveSprite(Drunk_Driver)
+let Spooky_ahh_pigeon = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
